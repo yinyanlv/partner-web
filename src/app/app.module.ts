46a6259dc9fe3b6cc43558b630a75c4c9b780fda
yinyanlv <yanlv.yin@gmail.com>
2018-04-26 12:17:46 +0800

    初始化项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import 'hammerjs';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
-
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
