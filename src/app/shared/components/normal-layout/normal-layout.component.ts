eae7aa9673218744c4537b96b3df6b39e7d2cc27
yinyanlv <yanlv.yin@gmail.com>
2018-05-14 14:14:56 +0800

    修改flex layout

--- src/app/shared/components/normal-layout/normal-layout.component.ts
+++ src/app/shared/components/normal-layout/normal-layout.component.ts
@@ -1,5 +1,7 @@
-import {Component, ViewChild, ComponentRef} from '@angular/core';
+import {Component, ViewChild, ComponentRef, NgZone} from '@angular/core';
 import { PerfectScrollbarConfigInterface, PerfectScrollbarDirective } from 'ngx-perfect-scrollbar';
+
+const SMALL_WIDTH_BREAKPOINT = '960px';
 
 @Component({
   selector: 'normal-layout',
@@ -11,11 +13,28 @@ import { PerfectScrollbarConfigInterface, PerfectScrollbarDirective } from 'ngx-
 export class NormalLayoutComponent {
 
   private scrollBarConfig: PerfectScrollbarConfigInterface = {};
+  private mqlEvent: MediaQueryList = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT})`);
 
   @ViewChild(PerfectScrollbarDirective) scrollBar: PerfectScrollbarDirective;
+
+  constructor(
+    private zone: NgZone
+  ) {
+
+    this.mqlEvent.addListener((mql) => {
+      zone.run(() => {
+        this.mqlEvent = mql;
+      });
+    })
+  }
 
   ngOnInit() {
 
   }
 
+  getNavMode() {
+console.log(this.mqlEvent.matches);
+console.log(this.mqlEvent.matches);
+    return this.mqlEvent.matches ? 'over' : 'side';
+  }
 }
