dd23caceb218e44dde2a0300f7e4e851e86a197c
bugong <1761869346@qq.com>
2018-07-11 23:44:58 +0800

    fix: prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiPrefix: 'http://localhost:8888/api'
-  // apiPrefix: 'http://118.31.54.173/api'
+  apiPrefix: 'http://118.31.54.173/api'
 };
